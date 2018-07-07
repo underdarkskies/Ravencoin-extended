24fc9d97ac00d644499f846e50fe0219e3ce635b
Under <35243703+underdarkskies@users.noreply.github.com>
2018-07-07 00:46:15 -0400

    Include p2pk into addressindex (#1839)
    * Add simple test for p2pk script
    
    refs #1767
    
    * Add p2pk transaction addresses indexing
    
    Addresses from p2pk transaction are now indexed by transations pubkeys
    hashes. Even if it isn't strict behavior and the p2pk are rare
    and obsolete, it is transaction that should be able to be looked up.
    
    fixes #1767
    
    * Add p2pk script checking tests
    
    refs #1767

--- src/script/script.h
+++ src/script/script.h
@@ -647,6 +647,9 @@ public:
     bool IsPayToWitnessScriptHash() const;
     bool IsWitnessProgram(int& version, std::vector<unsigned char>& program) const;
 
+    /** Used for obsolete pay-to-pubkey addresses indexing. */
+    bool IsPayToPublicKey() const;
+
     /** Called by IsStandardTx and P2SH/BIP62 VerifyScript (which makes it consensus-critical). */
     bool IsPushOnly(const_iterator pc) const;
     bool IsPushOnly() const;
